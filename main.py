6571fbb4e5a93f6ba79fd3d2b6e0d15b19cc034f
Filip <filip.dornik@gmail.com>
2025-01-23 19:25:51 +0100

    Verzia 2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+from dataclasses import asdict
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -14,4 +16,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+as
+dasdasdasd
+sadad
+asdict(asd)
